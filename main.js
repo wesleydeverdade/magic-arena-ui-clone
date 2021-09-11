f5226cdc6023203a22e4adc9f40f9acccdaeeb3d
wesleydeverdade <wesleymagalhaes5@hotmail.com>
2021-09-11 03:40:12 -0300

    dinamically card align

---
+++ main.js
@@ -0,0 +1,40 @@
+
+function resizeCards(){
+    let hand_player_cards = document.getElementsByClassName('card')
+    let rotate = Math.round(((hand_player_cards.length) + (hand_player_cards.length/2)) * -1) ;
+    // just in case
+    // let center_card = Math.round(hand_player_cards.length/2)-1; 
+    
+    // even
+    if(hand_player_cards.length % 2 === 0){
+        for (let index = 0; index < hand_player_cards.length; index++) {
+            let top = (Math.abs(rotate))*4;
+            hand_player_cards[index].style.transform = 'translateY(0px) rotate('+rotate+'deg)';     
+            hand_player_cards[index].style.top = top+'px';            
+            rotate+=3; 
+            // for balacing, both sides of hand with the same size 
+            if(rotate===0) rotate+=3;                   
+        } 
+    // odd    
+    }else{    
+        for (let index = 0; index < hand_player_cards.length; index++) {
+            let top = (Math.abs(rotate))*4;
+            hand_player_cards[index].style.transform = 'translateY(0px) rotate('+rotate+'deg)';     
+            hand_player_cards[index].style.top = top+'px';
+            rotate+=3;   
+        }
+    }
+
+}
+
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        resizeCards();
+    });    
+});
+
+// pass in the target node, as well as the observer options
+observer.observe(document.getElementById('player-hand'), { childList: true });
+
+resizeCards();
